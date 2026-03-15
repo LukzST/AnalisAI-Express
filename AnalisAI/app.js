1602021619afead44c007fc8c71fc7ff860234df
LukzST <lucaseduarte6@gmail.com>
2026-03-15 09:43:08 -0300

    add start command

--- AnalisAI/app.js
+++ AnalisAI/app.js
@@ -9,6 +9,7 @@ const flash = require('connect-flash');
 app.use(favicon(path.join(__dirname, 'Public', 'favicon.ico')));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('Public'));
 app.use(express.urlencoded({ extended: true }));
 
