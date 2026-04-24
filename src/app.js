befb6193ed1b9cf9ab5592e9e81e95f00b804eea
LukzST <lucaseduarte6@gmail.com>
2026-04-24 14:26:36 -0300

    fix structure, README.md and add .gitignore

--- src/app.js
+++ src/app.js
@@ -39,9 +39,10 @@ const upload = multer({
 });
 
 app.use(favicon(path.join(__dirname, 'Public', 'favicon.ico')));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(express.static('Public'));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
